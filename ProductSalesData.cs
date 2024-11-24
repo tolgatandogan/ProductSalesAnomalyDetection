43c9d94f2cdd42cd4ccf16a7f9e5850e0bf2c57a
TOLGA TANDOGAN <TOLGA.TANDOGAN@koton.com.tr>
2024-11-24 22:16:13 +0300

    Add project files.

---
+++ ProductSalesData.cs
@@ -0,0 +1,26 @@
+﻿// </SnippetAddUsing>
+using Microsoft.ML.Data;
+
+// </SnippetAddUsing>
+
+namespace ProductSalesAnomalyDetection
+{
+    // <SnippetDeclareTypes>
+    public class ProductSalesData
+    {
+        [LoadColumn(0)]
+        public string Month;
+
+        [LoadColumn(1)]
+        public float numSales;
+    }
+
+    public class ProductSalesPrediction
+    {
+        //vector to hold alert,score,p-value values
+        [VectorType(4)]
+        public double[] Prediction { get; set; }
+    }
+
+    // </SnippetDeclareTypes>
+}
